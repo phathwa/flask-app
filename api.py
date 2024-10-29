107faa39f850ce57b92e6ef104918bf42e0dd5d7
The Merchant <phathwa@gmail.com>
2024-10-29 20:00:22 +0200

    app test 2

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 api = FastAPI()
 
 
 @api.get("/")
 async def home():
     return {"message": "Hello World"}
+
+if __name__ == "__main___":
+    uvicorn.run("api:api", host="0.0.0.0", port=5000, reload=True)
